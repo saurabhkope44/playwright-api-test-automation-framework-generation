840450cbf6607911c78015b05a074be3e1a47ca6
saurabhkope44 <saurabhkope1644@gmail.com>
2026-09-19 23:46:45 +0530

    Delete dist directory

--- dist/Tests/User/User-GET.spec.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dynamicTestRunner_1 = require("../../src/generator/dynamicTestRunner");
-(0, dynamicTestRunner_1.registerApiTests)("User", "GET");
